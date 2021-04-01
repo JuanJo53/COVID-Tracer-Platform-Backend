c82586edfdab44b9f5a2074df667a1c57e742ec1
jantonio2 <jesusrochavedia@gmail.com>
2021-04-01 16:19:12 -0400

    Initial commit

---
+++ src/main/java/bo/ucb/edu/covid_tracer_platform_backend/CovidTracerPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.covid_tracer_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTracerPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTracerPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bo/ucb/edu/covid_tracer_platform_backend/CovidTracerPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.covid_tracer_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTracerPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
