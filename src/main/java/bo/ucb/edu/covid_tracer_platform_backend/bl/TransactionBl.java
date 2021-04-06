1c00744e80024475f9e742eaa51dfe6eedd0b7f4
jantonio2 <jesusrochavedia@gmail.com>
2021-04-06 19:28:59 -0400

    Transaction bl, dao, model and util done

---
+++ src/main/java/bo/ucb/edu/covid_tracer_platform_backend/bl/TransactionBl.java
@@ -0,0 +1,28 @@
+package bo.ucb.edu.covid_tracer_platform_backend.bl;
+
+import bo.ucb.edu.covid_tracer_platform_backend.dao.TransactionDao;
+import bo.ucb.edu.covid_tracer_platform_backend.model.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionBl {
+
+    private TransactionDao transactionDao;
+
+    @Autowired
+    public TransactionBl(TransactionDao transactionDao) {
+        this.transactionDao = transactionDao;
+    }
+
+    public Transaction createTransaction (Transaction transaction) {
+        // Registramos la transacción en la Base de Datos
+        this.transactionDao.create(transaction);
+
+        // Obtenemos la llave primaria generada
+        Integer lastPrimaryKey = this.transactionDao.getLastInsertId();
+
+        transaction.setTxId(lastPrimaryKey);
+        return transaction;
+    }
+}
---
+++ src/main/java/bo/ucb/edu/covid_tracer_platform_backend/dao/TransactionDao.java
@@ -0,0 +1,11 @@
+package bo.ucb.edu.covid_tracer_platform_backend.dao;
+
+import bo.ucb.edu.covid_tracer_platform_backend.model.Transaction;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TransactionDao {
+    public Integer create(Transaction transaction);
+
+    public Integer getLastInsertId();
+}
