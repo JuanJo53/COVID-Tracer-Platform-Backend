b90d1142fda2856f9886208b5f18a3523f534771
jantonio2 <jesusrochavedia@gmail.com>
2021-04-07 08:52:59 -0400

    Create user done

---
+++ src/main/java/bo/ucb/edu/covid_tracer_platform_backend/dto/UserRequest.java
@@ -0,0 +1,83 @@
+package bo.ucb.edu.covid_tracer_platform_backend.dto;
+
+public class UserRequest {
+    private String firstName;
+    private String firstSurname;
+    private String secondSurname;
+    private String email;
+    private String userName;
+    private String password;
+    private Integer role;
+
+    public UserRequest() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstSurname() {
+        return firstSurname;
+    }
+
+    public void setFirstSurname(String firstSurname) {
+        this.firstSurname = firstSurname;
+    }
+
+    public String getSecondSurname() {
+        return secondSurname;
+    }
+
+    public void setSecondSurname(String secondSurname) {
+        this.secondSurname = secondSurname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "firstName='" + firstName + '\'' +
+                ", firstSurname='" + firstSurname + '\'' +
+                ", secondSurname='" + secondSurname + '\'' +
+                ", email='" + email + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
