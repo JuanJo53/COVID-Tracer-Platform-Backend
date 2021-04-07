34a8c17755ca70fe36870ccd370dbb0f0a2873ca
jantonio2 <jesusrochavedia@gmail.com>
2021-04-06 20:11:53 -0400

    Role, User and UserRole models done

---
+++ src/main/java/bo/ucb/edu/covid_tracer_platform_backend/model/UserRole.java
@@ -0,0 +1,64 @@
+package bo.ucb.edu.covid_tracer_platform_backend.model;
+
+public class UserRole {
+    private Integer userRoleId;
+    private Integer userId;
+    private Integer roleId;
+    private Integer status;
+    private Transaction transaction;
+
+    public UserRole() {
+        transaction = new Transaction();
+    }
+
+    public Integer getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Integer userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "userRoleId=" + userRoleId +
+                ", userId=" + userId +
+                ", roleId=" + roleId +
+                ", status=" + status +
+                ", transaction=" + transaction +
+                '}';
+    }
+}
