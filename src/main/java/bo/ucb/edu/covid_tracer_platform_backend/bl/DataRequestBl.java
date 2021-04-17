56cb2af890dadb61e59356e87ce4466fac576d33
jantonio2 <jesusrochavedia@gmail.com>
2021-04-17 19:44:06 -0400

    Data upload list for admin done

---
+++ src/main/java/bo/ucb/edu/covid_tracer_platform_backend/bl/DataRequestBl.java
@@ -0,0 +1,31 @@
+package bo.ucb.edu.covid_tracer_platform_backend.bl;
+
+import bo.ucb.edu.covid_tracer_platform_backend.dao.DataRequestDao;
+import bo.ucb.edu.covid_tracer_platform_backend.dao.TransactionDao;
+import bo.ucb.edu.covid_tracer_platform_backend.dto.DataRequestList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DataRequestBl {
+    private DataRequestDao dataRequestDao;
+    private TransactionDao transactionDao;
+
+    @Autowired
+    public DataRequestBl(DataRequestDao dataRequestDao, TransactionDao transactionDao) {
+        this.dataRequestDao = dataRequestDao;
+        this.transactionDao = transactionDao;
+    }
+
+    public List<DataRequestList> dataRequestList(Integer page, Integer size){
+        List<DataRequestList> list = dataRequestDao.dataRequestWorldList(page, size);
+        return list;
+    }
+
+    public List<DataRequestList> dataRequestDepartmentList(Integer page, Integer size){
+        List<DataRequestList> list = dataRequestDao.dataRequestDepartmentList(page, size);
+        return list;
+    }
+}
